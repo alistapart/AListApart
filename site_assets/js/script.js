22bdafbe014ab7f63b4e549ebfab66bafd6dfe8e
Tim Murtaugh <tim.murtaugh@ogilvy.com>
2014-01-14 16:34:18 -0500

    ref #5: set up auto-expanding textareas
    The script I added: https://github.com/murtaugh/auto-resizing-textareas

--- site_assets/js/script.js
+++ site_assets/js/script.js
@@ -303,6 +303,12 @@ $(document).ready(function(){
 		$(".comment").CommentEditor();
 
 	});
+	
+	$('textarea[data-autoresize]').on('input propertychange', function() {
+	
+		autoResize($(this));
+	
+	});
 				
 });
 
@@ -1061,4 +1067,58 @@ $.fn.CommentEditor = function(options) {
 				getHash = hash;
 			});
 		}
+};
+
+/*
+		Functions for auto-resizing textareas.
+		Based almost entirely on John Long's excellent autogrow plugin (except I un-plugin-ifyed it):
+		https://gist.github.com/jlong/2127634
+		
+		For a more consistent user experience you should apply resize: none to auto-resizing textareas in your CSS.
+		The plugin needs to apply it on its own, so it's better that it not be there at all. (That said, you probably
+		want to apply that style only when JS is avaialble.)
+*/
+
+var properties = ['-webkit-appearance','-moz-appearance','-o-appearance','appearance','font-family','font-size','font-weight','font-style','border','border-top','border-right','border-bottom','border-left','box-sizing','padding','padding-top','padding-right','padding-bottom','padding-left','min-height','max-height','line-height'],		escaper = $('<span />');
+
+function escape(string) {
+		return escaper.text(string).text().replace(/\n/g, '<br>');
+};
+
+function autoResize(which) {
+		
+		if (!which.data('autogrow-applied')) {
+		
+				var textarea = which, initialHeight = textarea.innerHeight(), expander = $('<div />'), timer = null;
+
+				// Setup expander
+				expander.css({'position': 'absolute', 'visibility': 'hidden', 'bottom': '110%'})
+				$.each(properties, function(i, p) { expander.css(p, textarea.css(p)); });
+				textarea.after(expander);
+
+				// Setup textarea
+				textarea.css({'overflow-y': 'hidden', 'resize': 'none', 'box-sizing': 'border-box'});
+
+				// Sizer function
+				function sizeTextarea() {
+						clearTimeout(timer);
+						timer = setTimeout(function() {
+								var value = escape(textarea.val()) + '<br>z';
+								expander.html(value);
+								expander.css('width', textarea.innerWidth() + 2 + 'px');
+								textarea.css('height', Math.max(expander.innerHeight(), initialHeight) + 2 + 'px');
+						}, 0); // throttle by 100ms 
+				}
+
+				// Bind sizer to IE 9+'s input event and Safari's propertychange event
+				textarea.on('input.autogrow propertychange.autogrow', sizeTextarea);
+
+				// Set the initial size
+				sizeTextarea();
+
+				// Record autogrow applied
+				textarea.data('autogrow-applied', true);
+				
+		};
+		
 };
