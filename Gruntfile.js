21450e3634d21fe9840544fd57523fc61f8ed83c
Ethan Marcotte <me@ethanmarcotte.com>
2014-09-04 17:35:04 -0400

    critical CSS, add JS to concat, add watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,16 +9,23 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
+		domain: 'local.ala',
 		banner: '/*! A List Apart - ' +
 				'v<%= grunt.template.today("yyyymmdd") %>\n' +
 				'Copyright © <%= grunt.template.today("yyyy") %> A List Apart */',
 		criticalcss: {
+			article: {
+				options: {
+					outputfile : 'site_assets/css/dist/critical-article.css',
+					filename : 'all.dist.css',
+					url : 'http://<%= domain %>/article/responsive-web-design?page_static=true'
+				}
+			},
 			home: {
 				options: {
-					outputfile : '_dist/critical-home.css',
-					filename : 'all.css',
-					url : 'http://local.ala/?static=true'
+					outputfile : 'site_assets/css/dist/critical-home.css',
+					filename : 'all.dist.css',
+					url : 'http://<%= domain %>/?page_static=true'
 				}
 			}
 		},
@@ -48,20 +55,51 @@ module.exports = function(grunt) {
 				],
 				dest: 'site_assets/css/all.dist.css'
 			},
+			js_initial: {
+				src: [
+					'site_assets/js/libs/modernizr-custom.js',
+					'site_assets/js/initial.js'
+				],
+				dest: 'site_assets/js/dist/initial.js'
+			},
 			js_main: {
 				src: [
+					'site_assets/js/libs/jquery-1.8.2.min.js',
 					'site_assets/js/libs/matchmedia.js',
 					'site_assets/js/libs/picturefill.js',
 					'site_assets/js/libs/breakpoints.js',
-					'site_assets/js/libs/jquery-1.8.2.min.js',
 					'site_assets/js/script.js'
 				],
-				dest: 'site_assets/js/all.dist.js'
+				dest: 'site_assets/js/dist/main.js'
+			},
+			js_full: {
+				src: [
+					'<%= concat.js_initial.dest %>',
+					'<%= concat.js_main.dest %>'
+				],
+				dest: 'site_assets/js/dist/full.js'
+			}
+		},
+		watch: {
+			all: {
+				files: [
+					'!site_assets/css/dist/**/*',
+					'!site_assets/js/dist/**/*',
+					'!<%= concat.css_main.dest %>',
+					'!<%= concat.js_main.dest %>',
+					'site_assets/css/**/*',
+					'site_assets/js/**/*'
+					],
+				tasks: 'default'
+			},
+			grunt: {
+				files: ['Gruntfile.js'],
+				tasks: []
 			}
 		}
 	});
 
-	grunt.registerTask( "default", [ "concat", "cssmin" ] )
+	grunt.registerTask( "default", [ "concat", "cssmin", "criticalcss" ] )
 
 	// grunt.registerTask( "default", [ "concat", "cssmin", "svgmin", "uglify", "grunticon", "shell:jekyllDev" ] );
 	// grunt.registerTask( "css", [ "concat", "cssmin", "shell:jekyllDev" ] );
