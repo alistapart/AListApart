ce917d80ca292d3d100a123b9c8f20aab42cad3b
Tim Murtaugh <tim@o2b.net>
2015-07-16 14:50:33 -0400

    test an in-prism method for a tab-size polyfill

--- assets/js/libs/prism.js
+++ assets/js/libs/prism.js
@@ -11,10 +11,10 @@ var lang = /\blang(?:uage)?-(?!\*)(\w+)\b/i;
 
 var _ = self.Prism = {
 	util: {
-		type: function (o) { 
+		type: function (o) {
 			return Object.prototype.toString.call(o).match(/\[object (\w+)\]/)[1];
 		},
-		
+
 		// Deep clone a language definition (e.g. to extend it)
 		clone: function (o) {
 			var type = _.util.type(o);
@@ -22,66 +22,66 @@ var _ = self.Prism = {
 			switch (type) {
 				case 'Object':
 					var clone = {};
-					
+
 					for (var key in o) {
 						if (o.hasOwnProperty(key)) {
 							clone[key] = _.util.clone(o[key]);
 						}
 					}
-					
+
 					return clone;
-					
+
 				case 'Array':
 					return o.slice();
 			}
-			
+
 			return o;
 		}
 	},
-	
+
 	languages: {
 		extend: function (id, redef) {
 			var lang = _.util.clone(_.languages[id]);
-			
+
 			for (var key in redef) {
 				lang[key] = redef[key];
 			}
-			
+
 			return lang;
 		},
-		
+
 		// Insert a token before another token in a language literal
 		insertBefore: function (inside, before, insert, root) {
 			root = root || _.languages;
 			var grammar = root[inside];
 			var ret = {};
-				
+
 			for (var token in grammar) {
-			
+
 				if (grammar.hasOwnProperty(token)) {
-					
+
 					if (token == before) {
-					
+
 						for (var newToken in insert) {
-						
+
 							if (insert.hasOwnProperty(newToken)) {
 								ret[newToken] = insert[newToken];
 							}
 						}
 					}
-					
+
 					ret[token] = grammar[token];
 				}
 			}
-			
+
 			return root[inside] = ret;
 		},
-		
+
 		// Traverse a language definition with Depth First Search
 		DFS: function(o, callback) {
 			for (var i in o) {
 				callback.call(o, i, o[i]);
-				
+
 				if (_.util.type(o) === 'Object') {
 					_.languages.DFS(o[i], callback);
 				}
@@ -96,15 +96,15 @@ var _ = self.Prism = {
 			_.highlightElement(element, async === true, callback);
 		}
 	},
-		
+
 	highlightElement: function(element, async, callback) {
 		// Find language
 		var language, grammar, parent = element;
-		
+
 		while (parent && !lang.test(parent.className)) {
 			parent = parent.parentNode;
 		}
-		
+
 		if (parent) {
 			language = (parent.className.match(lang) || [,''])[1];
 			grammar = _.languages[language];
@@ -113,48 +113,55 @@ var _ = self.Prism = {
 		if (!grammar) {
 			return;
 		}
-		
+
 		// Set language on the element, if not present
 		element.className = element.className.replace(lang, '').replace(/\s+/g, ' ') + ' language-' + language;
-		
+
 		// Set language on the parent, for styling
 		parent = element.parentNode;
-		
+
 		if (/pre/i.test(parent.nodeName)) {
-			parent.className = parent.className.replace(lang, '').replace(/\s+/g, ' ') + ' language-' + language; 
+			parent.className = parent.className.replace(lang, '').replace(/\s+/g, ' ') + ' language-' + language;
 		}
 
 		var code = element.textContent;
-		
+
 		if(!code) {
 			return;
 		}
-		
+
 		code = code.replace(/&/g, '&amp;').replace(/</g, '&lt;')
 		           .replace(/>/g, '&gt;').replace(/\u00a0/g, ' ');
 		//console.time(code.slice(0,50));
-		
+
+		var tabSize = 4;
+		var testTabSize = document.createElement('i');
+		if(testTabSize.style.tabSize !== '' && testTabSize.style.MozTabSize !== '' && testTabSize.style.oTabSize !== '') {
+			console.log(tabSize.style);
+		   code = code.replace(/\t/g, repeat(" ", tabSize));
+		}
+
 		var env = {
 			element: element,
 			language: language,
 			grammar: grammar,
 			code: code
 		};
-		
+
 		_.hooks.run('before-highlight', env);
-		
+
 		if (async && self.Worker) {
-			var worker = new Worker(_.filename);	
-			
+			var worker = new Worker(_.filename);
+
 			worker.onmessage = function(evt) {
 				env.highlightedCode = Token.stringify(JSON.parse(evt.data));
 				env.element.innerHTML = env.highlightedCode;
-				
+
 				callback && callback.call(env.element);
 				//console.timeEnd(code.slice(0,50));
 				_.hooks.run('after-highlight', env);
 			};
-			
+
 			worker.postMessage(JSON.stringify({
 				language: env.language,
 				code: env.code
@@ -163,61 +170,61 @@ var _ = self.Prism = {
 		else {
 			env.highlightedCode = _.highlight(env.code, env.grammar)
 			env.element.innerHTML = env.highlightedCode;
-			
+
 			callback && callback.call(element);
-			
+
 			_.hooks.run('after-highlight', env);
 			//console.timeEnd(code.slice(0,50));
 		}
 	},
-	
+
 	highlight: function (text, grammar) {
 		return Token.stringify(_.tokenize(text, grammar));
 	},
-	
+
 	tokenize: function(text, grammar) {
 		var Token = _.Token;
-		
+
 		var strarr = [text];
-		
+
 		var rest = grammar.rest;
-		
+
 		if (rest) {
 			for (var token in rest) {
 				grammar[token] = rest[token];
 			}
-			
+
 			delete grammar.rest;
 		}
-								
+
 		tokenloop: for (var token in grammar) {
 			if(!grammar.hasOwnProperty(token) || !grammar[token]) {
 				continue;
 			}
-			
-			var pattern = grammar[token], 
+
+			var pattern = grammar[token],
 				inside = pattern.inside,
 				lookbehind = !!pattern.lookbehind || 0;
-			
+
 			pattern = pattern.pattern || pattern;
-			
-			for (var i=0; i<strarr.length; i++) { // DonÕt cache length as it changes during the loop
-				
+
+			for (var i=0; i<strarr.length; i++) { // Donï¿½t cache length as it changes during the loop
+
 				var str = strarr[i];
-				
+
 				if (strarr.length > text.length) {
 					// Something went terribly wrong, ABORT, ABORT!
 					break tokenloop;
 				}
-				
+
 				if (str instanceof Token) {
 					continue;
 				}
-				
+
 				pattern.lastIndex = 0;
-				
+
 				var match = pattern.exec(str);
-				
+
 				if (match) {
 					if(lookbehind) {
 						lookbehind = match[1].length;
@@ -228,22 +235,22 @@ var _ = self.Prism = {
 					    len = match.length,
 					    to = from + len,
 						before = str.slice(0, from + 1),
-						after = str.slice(to + 1); 
+						after = str.slice(to + 1);
 
 					var args = [i, 1];
-					
+
 					if (before) {
 						args.push(before);
 					}
-					
+
 					var wrapped = new Token(token, inside? _.tokenize(match, inside) : match);
-					
+
 					args.push(wrapped);
-					
+
 					if (after) {
 						args.push(after);
 					}
-					
+
 					Array.prototype.splice.apply(strarr, args);
 				}
 			}
@@ -251,25 +258,25 @@ var _ = self.Prism = {
 
 		return strarr;
 	},
-	
+
 	hooks: {
 		all: {},
-		
+
 		add: function (name, callback) {
 			var hooks = _.hooks.all;
-			
+
 			hooks[name] = hooks[name] || [];
-			
+
 			hooks[name].push(callback);
 		},
-		
+
 		run: function (name, env) {
 			var callbacks = _.hooks.all[name];
-			
+
 			if (!callbacks || !callbacks.length) {
 				return;
 			}
-			
+
 			for (var i=0, callback; callback = callbacks[i++];) {
 				callback(env);
 			}
@@ -286,15 +293,15 @@ Token.stringify = function(o) {
 	if (typeof o == 'string') {
 		return o;
 	}
-	
+
 	if (Object.prototype.toString.call(o) == '[object Array]') {
 		for (var i=0; i<o.length; i++) {
 			o[i] = Token.stringify(o[i]);
 		}
-		
+
 		return o.join('');
 	}
-	
+
 	var env = {
 		type: o.type,
 		content: Token.stringify(o.content),
@@ -302,21 +309,21 @@ Token.stringify = function(o) {
 		classes: ['token', o.type],
 		attributes: {}
 	};
-	
+
 	if (env.type == 'comment') {
 		env.attributes['spellcheck'] = 'true';
 	}
-	
+
 	_.hooks.run('wrap', env);
-	
+
 	var attributes = '';
-	
+
 	for (var name in env.attributes) {
 		attributes += name + '="' + (env.attributes[name] || '') + '"';
 	}
-	
+
 	return '<' + env.tag + ' class="' + env.classes.join(' ') + '" ' + attributes + '>' + env.content + '</' + env.tag + '>';
-	
+
 };
 
 if (!self.document) {
@@ -325,11 +332,11 @@ if (!self.document) {
 		var message = JSON.parse(evt.data),
 		    lang = message.language,
 		    code = message.code;
-		
+
 		self.postMessage(JSON.stringify(_.tokenize(code, _.languages[lang])));
 		self.close();
 	}, false);
-	
+
 	return;
 }
 
@@ -340,7 +347,7 @@ script = script[script.length - 1];
 
 if (script) {
 	_.filename = script.src;
-	
+
 	if (document.addEventListener && !script.hasAttribute('data-manual')) {
 		document.addEventListener('DOMContentLoaded', _.highlightAll);
 	}
@@ -375,7 +382,7 @@ Prism.languages.markup = {
 					'namespace': /^[\w-]+?:/
 				}
 			}
-			
+
 		}
 	},
 	'entity': /&amp;#?[\da-z]{1,8};/gi
