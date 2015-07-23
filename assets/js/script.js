fa66e84108c8f70a47fa585ed87e3e32ca49985b
Tim Murtaugh <tim@o2b.net>
2015-07-23 17:38:11 -0400

    i am the tabmaster

--- assets/js/script.js
+++ assets/js/script.js
@@ -343,6 +343,30 @@ $(document).ready(function(){
 
 	});
 
+	var tabMaster = false;
+
+	$('body').on('focus', '[data-tabmaster]', function() {
+
+		tabMaster = $(this).attr('data-tabmaster');
+
+	});
+
+	$('body').on('keydown', '[data-tabto]', function(e) {
+
+		var target = $(this).attr('data-tabto');
+
+		if (e.which == 9 && !e.shiftKey && tabMaster == "true") {
+			e.preventDefault();
+			console.log('tab detected');
+			$('#' + target).focus();
+		} else if (e.shiftKey) {
+
+			tabMaster = false;
+
+		};
+
+	});
+
 	var emailFilter = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
 
 	$("form#email-subscribe").submit(function(e) {
