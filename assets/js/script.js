6486237e3a05948c6083b52369f82d7eae137293
Tim Murtaugh <tim@o2b.net>
2015-07-27 15:31:42 -0400

    call the job board first, since it fails less often

--- assets/js/script.js
+++ assets/js/script.js
@@ -439,9 +439,9 @@ $(document).ready(function(){
 
 	*/
 
-	postscribe('#deck-target', '<div class="deck-inner"><script src=http://www.northmay.com/deck/deckAL_js.php?' + (new Date().getTime()) + '><\/script><p><a href="http://www.coudal.com/deck/">Ad via The Deck</a></p></div>');
-
 	postscribe('#job-board-target', '<h4>Job Board</h4><script src="https://weworkremotely.com/jobs/random.js"><\/script><p class="board-link">Job listings via <a href="https://weworkremotely.com/?source=ala">We Work Remotely</a></p>');
+
+	postscribe('#deck-target', '<div class="deck-inner"><script src=http://www.northmay.com/deck/deckAL_js.php?' + (new Date().getTime()) + '><\/script><p><a href="http://www.coudal.com/deck/">Ad via The Deck</a></p></div>');
 
 });
 
