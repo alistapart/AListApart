b56cc988a9673565470fece5c9f2cdbf780dfcc2
michellekondou <michellekondou@gmail.com>
2015-08-14 15:15:50 +0300

    create postscribe-init.js file

---
+++ assets/js/postscribe-init.js
@@ -0,0 +1,11 @@
+		
+		postscribe('#job-board-target', '<h4>Job Board</h4><script src="https://weworkremotely.com/jobs/random.js"><\/script><p class="board-link">Job listings via <a href="https://weworkremotely.com/?source=ala">We Work Remotely</a></p>');
+        postscribe('#deck-target', '<div class="deck-inner"><script src=http://www.northmay.com/deck/deckAL_js.php?' + (new Date().getTime()) + '><\/script><p><a href="http://www.coudal.com/deck/">Ad via The Deck</a></p></div>', {
+	            error: function(e) {
+	                console.log(e);
+	            },
+	            done: function() {
+	                console.info('the deck script has been delivered.');
+	            }
+	        });
+ 
