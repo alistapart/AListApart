d49f967b1f9e55515f187e8ad0753e93d407243b
michellekondou <michellekondou@gmail.com>
2015-08-22 22:49:03 +0300

    modal style changes

--- assets/js/ajax-functions.js
+++ assets/js/ajax-functions.js
@@ -1,43 +1,42 @@
+
 // Initiating AlaAuth object to hold form related functions
 var AlaAuth = {
 
 	// Create a simple modal for form success / error msgs.
 	customModal: function(){
 		
-		$("#overlay").remove();
-		$("html").append("<div id='overlay' style='height:"+$(document).height()+"px'></div>");
+		//$("#overlay").remove();
+		//$("html").append("<div id='overlay' style='height:"+$(document).height()+"px'></div>");
 		
 		$(document).on('keyup click', function(e){ //click anywhere to close the modal, or hit any key
 		    var container = $(".custom-modal");
-		    if (!container.is(e.target) // if the target of the click isn't the container
-		        && container.has(e.target).length === 0) // nor a descendant of the container
-		    {
+
+		    // if (!container.is(e.target) // if the target of the click isn't the container
+		    //     && container.has(e.target).length === 0) // nor a descendant of the container
+		    // {
 		        container.hide();
-		        $("#overlay").height('0').hide();
-		    }
+		        //$("#overlay").height('0').hide();
+		    // }
 		});
 
 	}, // end customModal	
 
 	appendModal: function(elementID, msg){
 
-			AlaAuth.customModal();
-			elementID.append("<div class='custom-modal'>"+msg+"</div>");
- 
+		AlaAuth.customModal();
+		elementID.append("<div class='custom-modal'>"+msg+"</div>");
+
 	} // end appendModal 
 
 }; //end AlaAuth object
 
-
-// function request_pass_msg(){
-// 	AlaAuth.appendModal($('.commenter-signin'), 'asdasd');
-// }
-
 function alert_msg(elementID, msg){
-
 	AlaAuth.customModal();
-	elementID.append("<div class='custom-modal'><i class='laurel-small'></i>"+msg+''+'<p class="note">Press any key or click anywhere outside this message to make it dissappear</p>'+"</div>");
-
+	$(".custom-modal").remove();
+	elementID.append("<div class='custom-modal'><div class='container'><i class='laurel-small'></i>"+msg+''+'<p class="note">X Press any key or click anywhere to close this</p>'+"</div></div>");
+	var innerEl = $(".custom-modal .container");
+	var innerElHeight = innerEl.outerHeight();
+	innerEl.css('margin-top', '-'+innerElHeight/2+'px');
 }	
 
  
