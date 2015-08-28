6b446d9ccfbbacb07f2239ce32da49cb9c08409a
michellekondou <michellekondou@gmail.com>
2015-08-28 16:53:49 +0300

    click the modal anywhere to close

--- assets/js/ajax-functions.js
+++ assets/js/ajax-functions.js
@@ -11,12 +11,12 @@ var AlaAuth = {
 		$(document).on('keyup click', function(e){ //click anywhere to close the modal, or hit any key
 		    var container = $(".custom-modal");
 
-		    if (!container.is(e.target) // if the target of the click isn't the container
-		        && container.has(e.target).length === 0) // nor a descendant of the container
-		    {
+		    // if (!container.is(e.target) // if the target of the click isn't the container
+		    //     && container.has(e.target).length === 0) // nor a descendant of the container
+		    // {
 		        container.hide();
 		        //$("#overlay").height('0').hide();
-		    }
+		    // }
 		});
 
 	}, // end customModal	
