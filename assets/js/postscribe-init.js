6557e36d98fd25baf72222eabdb55107fb9360b4
Tim Murtaugh <tim@o2b.net>
2016-09-15 14:25:23 -0400

    remove WWR text

--- assets/js/postscribe-init.js
+++ assets/js/postscribe-init.js
@@ -1,5 +1,5 @@
 
-		postscribe('#job-board-target', '<h4>Job Board</h4><a href="https://weworkremotely.com/?source=ala"><img src="/components/assets/img/we-work-remotely.jpg" alt="We Work Remotely"></a><script src="https://weworkremotely.com/jobs/random.js"><\/script><p class="board-link">Job listings via <a href="https://weworkremotely.com/?source=ala">We Work Remotely</a></p>', {
+		postscribe('#job-board-target', '<h4>Job Board</h4><a href="https://weworkremotely.com/?source=ala"><img src="/components/assets/img/we-work-remotely.jpg" alt="Job listings via We Work Remotely"></a><script src="https://weworkremotely.com/jobs/random.js"><\/script>', {
 	            error: function(e) {
 	                console.log(e);
 	            }
